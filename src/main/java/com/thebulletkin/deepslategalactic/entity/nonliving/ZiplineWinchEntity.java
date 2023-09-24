5dc5fe037efc93c7b9f19635faf8f4bbb92dcbe4
Oliver Warry <oliver@outspaceme.com>
2023-09-24 22:10:36 +0100

    It took far too long to figure out how to get the interaction to work. I'm sane now

--- src/main/java/com/thebulletkin/deepslategalactic/entity/nonliving/ZiplineWinchEntity.java
+++ src/main/java/com/thebulletkin/deepslategalactic/entity/nonliving/ZiplineWinchEntity.java
@@ -2,29 +2,22 @@ package com.thebulletkin.deepslategalactic.entity.nonliving;
 
 import com.thebulletkin.deepslategalactic.DeepSlateGalactic;
 import com.thebulletkin.deepslategalactic.entity.DSGEntities;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.vehicle.Boat;
-import net.minecraft.world.entity.vehicle.DismountHelper;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.Vec3;
-import org.jetbrains.annotations.Nullable;
 
-public class ZiplineWinchEntity extends Entity implements PlayerRideable {
+public class ZiplineWinchEntity extends Entity {
 
 
     public ZiplineWinchEntity(EntityType<?> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
     }
 
-    public ZiplineWinchEntity(Level pLevel, double pX, double pY, double pZ) {
-
+    public ZiplineWinchEntity(Level pLevel, double pX, double pY, double pZ){
         this(DSGEntities.ZIPLINE_WINCH.get(), pLevel);
         this.setPos(pX, pY, pZ);
         this.xo = pX;
@@ -37,7 +30,6 @@ public class ZiplineWinchEntity extends Entity implements PlayerRideable {
 
     }
 
-
     @Override
     public InteractionResult interact(Player pPlayer, InteractionHand pHand) {
         DeepSlateGalactic.LOGGER.info("interacted with");
@@ -47,47 +39,11 @@ public class ZiplineWinchEntity extends Entity implements PlayerRideable {
         }
 
         return InteractionResult.SUCCESS;
-
-    }
-
-
-    @Override
-    public double getPassengersRidingOffset() {
-        return 0D;
-    }
-
-    @Nullable
-    @Override
-    public LivingEntity getControllingPassenger() {
-        return ((LivingEntity) this.getFirstPassenger());
     }
 
     @Override
-    public Vec3 getDismountLocationForPassenger(LivingEntity pLivingEntity) {
-        Direction direction = this.getMotionDirection();
-        if (direction.getAxis() != Direction.Axis.Y) {
-            int[][] offsets = DismountHelper.offsetsForDirection(direction);
-            BlockPos blockpos = this.blockPosition();
-            BlockPos.MutableBlockPos blockpos$mutableblockpos = new BlockPos.MutableBlockPos();
-
-            for (Pose pose : pLivingEntity.getDismountPoses()) {
-                AABB aabb = pLivingEntity.getLocalBoundsForPose(pose);
-
-                for (int[] offset : offsets) {
-                    blockpos$mutableblockpos.set(blockpos.getX() + offset[0], blockpos.getY(), blockpos.getZ() + offset[1]);
-                    double d0 = this.level().getBlockFloorHeight(blockpos$mutableblockpos);
-                    if (DismountHelper.isBlockFloorValid(d0)) {
-                        Vec3 vec3 = Vec3.upFromBottomCenterOf(blockpos$mutableblockpos, d0);
-                        if (DismountHelper.canDismountTo(this.level(), pLivingEntity, aabb.move(vec3))) {
-                            pLivingEntity.setPose(pose);
-                            return vec3;
-                        }
-                    }
-                }
-            }
-        }
-
-        return super.getDismountLocationForPassenger(pLivingEntity);
+    public boolean isPickable() {
+        return true;
     }
 
     @Override
@@ -104,6 +60,4 @@ public class ZiplineWinchEntity extends Entity implements PlayerRideable {
     protected void addAdditionalSaveData(CompoundTag pCompound) {
 
     }
-
-
 }
